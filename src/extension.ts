dc897c249f640e903c628bf9b13fb090a1650ff4
grays.chen <grays.chen@dfgroup.pro>
2021-03-16 19:55:30 +0800

    Update:add get template from github

--- src/extension.ts
+++ src/extension.ts
@@ -2,7 +2,7 @@
 // Import the module and reference it with the alias vscode in your code below
 import * as vscode from 'vscode';
 import * as path from 'path';
-const fs = require('fs');
+import dgit from '@dking/dgit';
 
 // this method is called when your extension is activated
 // your extension is activated the very first time the command is executed
@@ -17,12 +17,11 @@ export function activate(context: vscode.ExtensionContext) {
   // Now provide the implementation of the command with registerCommand
   // The commandId parameter must match the command field in package.json
   let disposable = vscode.commands.registerCommand(
-    'vscode-plugin-genius.watchdog',
+    'vscode-plugin-genius.createFolder',
     () => {
       // The code you place here will be executed every time your command is executed
 
       // Display a message box to the user
-      vscode.window.showWarningMessage('Hello may!');
 
       let folder = vscode.workspace.workspaceFolders?.[0] || '';
       let watcher: vscode.FileSystemWatcher = vscode.workspace.createFileSystemWatcher(
@@ -34,50 +33,51 @@ export function activate(context: vscode.ExtensionContext) {
 
       watcher.onDidCreate((e) => {
         let dir = vscode.Uri.file(`${e.path}`);
+        let relativePath = vscode.workspace.asRelativePath(dir, false);
         vscode.workspace.fs.stat(dir).then((stat) => {
           if (stat.type === 2) {
-            let fileIndex = vscode.Uri.file(`${e.path}/index.js`);
-            const templateDir = path.join(
-              context.extensionPath,
-              'src',
-              'template'
-            );
-            const fileToCopyPath: string[] = [];
-            const fileToCopy: string[] = [];
-            travel(templateDir, (pathname: string) => {
-              fileToCopyPath.push(pathname);
-              fileToCopy.push(path.basename(pathname));
-            });
-            fileToCopyPath.forEach((value, index) => {
-              let srcFile = vscode.Uri.file(value);
-              let dstFile = vscode.Uri.file(`${e.path}/${fileToCopy[index]}`);
-              vscode.workspace.fs.copy(srcFile, dstFile);
+            const includePath: string[] =
+              vscode.workspace.getConfiguration('genius').get('includePath') ||
+              [];
+            includePath.forEach((value) => {
+              const regExp = `^${escapeRegExp(value)}/\\w+`;
+              const res = new RegExp(regExp, 'g').test(relativePath);
+              if (res) {
+                const destPath = path.join(e.path.substr(1)); // 目标下载路径
+                const dgitOptions = {
+                  maxRetryCount: 3, // 网络问题下载失败时尝试最大重新下载次数
+                  parallelLimit: 10, // 并行下载个数
+                  log: false, // 是否开启内部日志
+                  logSuffix: '', // 日志前缀
+                  exclude: [], // 需要排除的文件路径,
+                  include: [] // 需要包含的文件路径
+                };
+                const link: string =
+                  vscode.workspace
+                    .getConfiguration('genius')
+                    .get('githubAddress') || '';
+                const githubLinkOption = {
+                  githubLink: link // 也可以直接指定github 需要下载路径的地址
+                };
+                (async () => {
+                  await dgit(githubLinkOption, destPath, dgitOptions);
+                })();
+              }
             });
           }
         });
-
-        vscode.window.showInformationMessage(`create ${e.path} success!`); //In my opinion this should be called
       });
-      // watcher.onDidDelete((e) => {
-      //   console.log(e);
-      //   vscode.window.showInformationMessage('delete applied!'); //In my opinion this should be called
-      // });
     }
   );
 
   context.subscriptions.push(disposable);
+  vscode.commands.executeCommand('vscode-plugin-genius.createFolder');
 }
 
 // this method is called when your extension is deactivated
 export function deactivate() {}
 
-function travel(dir: any, callback: any) {
-  fs.readdirSync(dir).forEach((file: any) => {
-    var pathname = path.join(dir, file);
-    if (fs.statSync(pathname).isDirectory()) {
-      travel(pathname, callback);
-    } else {
-      callback(pathname);
-    }
-  });
+function escapeRegExp(string: string) {
+  return string.replace(/[.*+?^${}()|[\]\\]/g, '\\$&');
+  //$&表示整个被匹配的字符串
 }
